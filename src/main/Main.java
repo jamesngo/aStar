b712df578d79ac4c90893e18d05e1c44e162f36f
James Ngo <James Ngo>
2016-03-01 17:28:07 -0800

    added board (represents graph) and main

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello World!");
+		int[] x = new int[1];
+		System.out.println(x[0]);
+	}
+}
